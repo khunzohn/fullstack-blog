d307b448a04a6d3031fd78346ee747977a8d4916
Khun Zohn <khunzohn@gmail.com>
2020-12-14 11:12:07 +0630

    finish exe 4.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,22 +2,23 @@ const blogsRouter = require('express').Router()
 const { response } = require('express')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res, next) => {
-    Blog.find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
-        .catch(error => next(error))
+blogsRouter.get('/', async (req, res, next) => {
+    try {
+        const blogs = await Blog.find({})
+        res.json(blogs)  
+    } catch(error) {
+        next(error)
+    }
 })
 
-blogsRouter.post('/', (req, res, next) => {
-    const blog = new Blog(req.body)
-
-    blog.save()
-        .then(result => {
-            res.status(201).json(result)
-        })
-        .catch(error => next(error))
+blogsRouter.post('/', async (req, res, next) => {
+    try {
+        const blog = new Blog(req.body)
+        const result = await blog.save()
+        res.status(201).json(result)        
+    } catch(e) {
+        next(e)
+    }
 })
 
 module.exports = blogsRouter
