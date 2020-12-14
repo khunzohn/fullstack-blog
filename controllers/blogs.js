d6916a41f8ec069adf4dfe7e2e2cd1f9b254db5b
Khun Zohn <khunzohn@gmail.com>
2020-12-14 13:38:12 +0630

    Use async-errors package to eliminate try/catch

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,54 +3,37 @@ const { response } = require("express");
 const Blog = require("../models/blog");
 const logger = require("../utils/logger");
 
-blogsRouter.get("/", async (req, res, next) => {
-  try {
-    const blogs = await Blog.find({});
-    res.json(blogs);
-  } catch (error) {
-    next(error);
-  }
+blogsRouter.get("/", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
 });
 
 blogsRouter.get("/:id", async (req, res, next) => {
-  try {
-    const id = req.params.id
-    const results = await Blog.find({_id: id});
-    if(results.length > 0) {
-      res.json(results[0]);
-    } else {
-      res.status(404).send({ error: "No blog found"})
-    }
-  } catch (error) {
-    next(error);
+  const id = req.params.id;
+  const results = await Blog.find({ _id: id });
+  if (results.length > 0) {
+    res.json(results[0]);
+  } else {
+    res.status(404).send({ error: "No blog found" });
   }
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-  try {
-    const body = req.body;
-    const blog = new Blog({
-      author: body.author,
-      title: body.title,
-      url: body.url,
-      likes: body.likes ? body.likes : 0,
-    });
-    const result = await blog.save();
-    res.status(201).json(result);
-  } catch (e) {
-    next(e);
-  }
+  const body = req.body;
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes ? body.likes : 0,
+  });
+  const result = await blog.save();
+  res.status(201).json(result);
 });
 
 blogsRouter.delete("/:id", async (req, res, next) => {
-  console.log("deleting...")
-  try {
-    const id = req.params.id
-    const result = await Blog.findByIdAndDelete(id)
-    res.status(204).end()
-  } catch(e) {
-    next(e)
-  }
-})
+  const id = req.params.id;
+  const result = await Blog.findByIdAndDelete(id);
+  res.status(204).end();
+});
 
 module.exports = blogsRouter;
